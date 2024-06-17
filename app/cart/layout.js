913be45452e7759fb3eb818d0e65d14c190e8f71
updaun <updauney@daum.net>
2024-06-17 16:22:26 +0900

    [feat] page routing, layout

---
+++ app/cart/layout.js
@@ -0,0 +1,11 @@
+
+export default function CartLayout({ children }) {
+  
+    return (
+      <div>
+        <p>현대카드 무이자이벤트중</p>
+        {children}
+      </div>
+    );
+  }
+  
---
+++ app/cart/payment/page.js
@@ -0,0 +1,10 @@
+
+export default function Payment() {
+  
+    return (
+      <div>
+        <h1 className="title">결제페이지입니다.</h1>
+      </div>
+    );
+  }
+  
